19d5738051373842bf83afab004422b64b55527a
ahokp <77104952+ahokp@users.noreply.github.com>
2021-01-21 20:41:47 +0200

    Added linear regression

---
+++ Linear regression/Regression Working File.py
@@ -0,0 +1,55 @@
+#This program uses linear regression algorithm to predict student grades
+
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
+
+data = pd.read_csv("student-mat.csv", sep=";")
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+
+predict = "G3"
+
+X = np.array(data.drop([predict], 1))
+Y = np.array((data[predict]))
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+
+"""
+#Train models
+best = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
+
+    if acc > best:
+        best = acc
+        #Save model
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+"""
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+print("Coefficient: ", linear.coef_)
+print("Intercept: ", linear.intercept_)
+
+predictions = linear.predict(x_test)
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+
+p = "G1"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
